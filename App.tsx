a52d0c6fa790dad5fc6257b102d420317d79b035
Matthew Weeks <weeksling@gmail.com>
2021-07-08 00:59:44 -0400

    Make koin screen

--- App.tsx
+++ App.tsx
@@ -1,24 +1,79 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import { Button, StyleSheet } from "react-native";
+import { View, Text } from "./components/Themed";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const useKoinsState = () => {
+  const [koins, setKoins] = React.useState<number>(0);
+
+  React.useEffect(() => {
+    AsyncStorage.getItem("kount").then((kount) => {
+      if (kount) {
+        setKoins(parseInt(kount));
+      }
+    });
+  }, []);
+
+  React.useEffect(() => {
+    AsyncStorage.setItem("kount", JSON.stringify(koins));
+  }, [koins]);
+
+  return {
+    koins,
+    incrementKoins: () => {
+      setKoins(koins + 10);
+    },
+  };
+};
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  const { koins, incrementKoins } = useKoinsState();
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        {/* <Navigation colorScheme={colorScheme} /> */}
         <StatusBar />
+        <View style={styles.container}>
+          <Text>You've earned {koins} koins!</Text>
+        </View>
+        <Button
+          style={styles.button}
+          onPress={incrementKoins}
+          title="Did you workout today?"
+        />
       </SafeAreaProvider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  button: {
+    height: 30,
+  },
+});
